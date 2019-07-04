82c662b377d8959e65bba2b834dab8a4226646ac
Keiron Stoddart <keiron.stoddart@gmail.com>
2019-07-04 13:01:59 -0400

    setting up framework

---
+++ app/routes.py
@@ -0,0 +1,10 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def cover():
+	return render_template('cover.html')
+
+@app.route('/content')
+def content():
+	return render_template('content.html')
