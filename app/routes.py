2b3181e44a0b789a1e4249eddcb90e1787bc6e21
Keiron Stoddart <keiron.stoddart@gmail.com>
2019-07-06 12:06:08 -0400

    modularizing content format

--- app/routes.py
+++ app/routes.py
@@ -12,6 +12,6 @@ def content():
 	return render_template('content.html')
 
 
-@app.route('/content/mexico')
-def mexico():
-	return render_template('content/mexico.html')
+@app.route('/content/the_data_science_stack')
+def the_data_science_stack():
+	return render_template('content/the_data_science_stack.html', title='The Data Science Stack')
